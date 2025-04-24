523663c49323cf4fefbe07fa95424eca35bd738f
pwcpextractorbot <pwcpextractorbot@gmail.com>
2025-04-24 08:37:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "27058143"
 api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
-bot_token = "7975806032:AAHKGknlm-YHiID7URXmomwGpQfU4quCEeQ"
+bot_token = "7975806032:AAEyCXWUeYYvSV-LZos1IQZBjVWp-2lCRpc"
 auth_users = [8144269730]
