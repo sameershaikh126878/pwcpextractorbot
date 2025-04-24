17068015de0aa9d9e1a57079ae3b712f38140e70
pwcpextractorbot <pwcpextractorbot@gmail.com>
2025-04-24 07:25:00 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "27058143"
 api_hash = "c569ea4f5ade3fc70d2bb1fd162d9dc9"
-bot_token = "7692348851:AAFauT5HaY-bllHEDe8pFcSfj_mvh8pTj2I"
-auth_users = [6148865829,5817712634]
+bot_token = "7762832960:AAFiaodZuSxCeKf57k4WA0-GNud1CFg87Nw"
+auth_users = [8144269730]
